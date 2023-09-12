bd5f752a1804aefca67cc8ad3509fbe138b2fb8b
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2023-09-12 14:35:39 +0300

    calculator
    калькулятор

---
+++ CalculatorDemo(V0.1).cpp
@@ -0,0 +1,113 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    //int a = INT32_MAX;
+    //int a2 = INT32_MIN;
+    //cout << a << " " << a2;
+
+    //cout << "\n";
+
+    //long long b = LONG_MAX;
+    //long long b2 = LONG_MIN;
+    //cout << b << " " << b2;
+
+    //cout << "\n";
+
+    //short c = SHRT_MAX;
+    //short c2 = SHRT_MIN;
+    //cout << c << " " << c2;
+
+    //cout << "\n";
+
+    //unsigned int d = INT32_MAX;
+    //unsigned int d2 = INT32_MIN;
+    //cout << d << " " << d2;
+
+    //cout << "\n";
+
+    //unsigned short q = USHRT_MAX;
+    //cout << q;
+
+    //cout << "\n";
+
+    //float e = FLT_MAX;
+    //float e2 = FLT_MIN;
+    //cout << e << " " << e2;
+
+    //cout << "\n";
+
+    //double f = DBL_MAX;
+    //double f2 = DBL_MIN;
+    //cout << f << " " << f2;
+
+    //cout << "\n";
+
+    //char g = CHAR_MAX;
+    //char g2 = CHAR_MIN;
+    //cout << g << " " << g2;
+
+    //cout << "\n";
+
+    //bool h = 1;
+    //bool h2 = 0;
+    //cout << h << " " << h2;
+
+    //cout << "\n";
+    //cout << "\n";
+
+    //cout << "type \t\t| byte\t\t| min\t\t| max\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "int \t\t|"<<sizeof(a) << "\t\t|" << a2 << "\t|" << a << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "longX2 \t\t|" << sizeof(b) << "\t\t|" << b2 << "\t|" << b << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "short \t\t|" << sizeof(c) << "\t\t|" << c2 << "\t\t|" << c << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "UNSint \t\t|" << sizeof(d) << "\t\t|" << d2 << "\t|" << d << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "UNSshort \t|" << sizeof(q) << "\t\t|" << 0 << "\t\t|" << q << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "float \t\t|" << sizeof(e) << "\t\t|" << e2 << "\t|" << e << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "dobule \t\t|" << sizeof(f) << "\t\t|" << f2 << "\t|" << f << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "char \t\t|" << sizeof(g) << "\t\t|" << g2 << "\t\t|" << g << "\n";
+    //cout << "--------------------------------------------------------------\n";
+    //cout << "bool \t\t|" << sizeof(h) << "\t\t|" << h2 << "\t\t|" << h << "\n";
+
+
+    //int  a, b;
+    //cout << "input a, b: ";
+    //cin >> a >> b;
+    //cout << a+b;
+
+    //cout << "\n"
+
+    //int a1, b1,c1;
+    //cout << "input a1, b1, c1: ";
+    //cin >> a1 >> b1 >> c1;
+    //cout << a1 + b1 + c1;
+
+    //int a, b;
+    //cout << "input a, b: ";
+    //cin >> a >> b;
+    //cout << "plus " << a + b << "\n";
+    //cout << "minus " << a - b << "\n";
+    //cout << "raznost " << a / b << "\n";
+    //cout << "umnoj " << a * b << "\n";
+
+    int a, b;
+    cout << "input a: ";
+    cin >> a;
+    cout << "input b: ";
+    cin >> b;
+    cout << a << "+" << b << "=" << a + b << "\n";
+    cout << a << "-" << b << "=" << a - b << "\n";
+    cout << a << "/" << b << "=" << a / b << "\n";
+    cout << a << "*" << b << "=" << a * b << "\n";
+}
