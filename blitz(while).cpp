4a45aae0c2c67ba36bd7ddf1157d6248c95ac5b7
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2023-10-24 12:18:33 +0300

    Add files via upload

---
+++ blitz(while).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	int a=0;
+//	while (a < 10)
+//		cout << ++a << " ";
+//
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	int a, b,c=0;
+//	cin >> a >> b;
+//	if (a > b) swap(a, b);
+//	while (c >= a) cout << c++ << " ";
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	int a,sum=0;
+//	while (true);
+//
+//}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, min;
+	cin >> a;
+	while (a != 0) {
+		if (min > a) min = a;
+		cin >> a;
+	}
+	cout << min;
+}
