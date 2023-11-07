e9fb480802ea4495393d03507022c2f96b525bac
DrizhenkoA <144795129+DrizhenkoA@users.noreply.github.com>
2023-11-07 14:45:02 +0300

    Add files via upload

---
+++ lesson07_11_23.cpp
@@ -0,0 +1,171 @@
+﻿#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	for (int i = 0; i < 4; i++) {
+//		cout << " " << endl;
+//		for (int j = 0; j < 5; j++)
+//			cout << "Hi ";
+//	}
+//
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a = '*';
+//	int n;
+//	cout << "Введите n - ";
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cout << " " << endl;
+//		for (int j = 0; j < n; j++) {
+//			if (i == 0 || i == n-1 || j == 0 || j == n-1) cout << a << " ";
+//			else cout << "  ";
+//		}
+//	}
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	int a, b;
+//	cin >> a >> b;
+//	int f = 1;
+//	int summ = 0;
+//	for (int j = a; j < b; j++) {
+//		for (int i = a; i < b; i++) {
+//			f *= i;
+//		}
+//		summ += f;
+//	}
+//	cout << summ;
+//
+//}
+
+//int main()
+//{
+//    int count = 0;
+//    int a, b;
+//    cin >> a >> b;
+//    bool flag;
+//    for (int i = a; i <= b; i++) {
+//        flag = true;
+//        for (int j = 2; j <= sqrt(i); j++) {
+//            if (i % j == 0) {
+//                flag = false;
+//                break;
+//            }
+//        }
+//        if (flag)count++;
+//    }
+//    cout << count;
+//}
+
+
+//int main() //вводит кол-во студентов и дисциплин, необходимо выставить по каждой дисциплине каждому студенту рандомную оценку от 2 до 5, вывести в таблице
+//{
+//	srand(time(NULL));
+//	setlocale(LC_ALL, "rus");
+//	int stud, les,count=0;
+//	float sum;
+//	cout << "Введите кол-во студентов - ";
+//	cin >> stud;
+//	cout << "Введите кол-во дисциплин - ";
+//	cin >> les;
+//	cout << "\trus\teng\tmath\tgeom\tphis\n";
+//	for (int i = 0; i < stud; i++) {
+//		cout << "N " << i + 1 << ":\t";
+//		sum = 0;
+//		for (int j = 0,k; j < les; j++) {
+//			k = 2 + rand() % (5 + 1 - 2);
+//			cout << k << "\t";
+//			sum += k;
+//		}
+//		if (sum / les >= 3 && sum / les <= 4) count++;
+//		cout << endl;
+//	}
+//	cout << count;
+//}
+
+//int main() //вводит число (мы не знаем скольки значное число) посчитать сумму цифр числа
+//{
+//	srand(time(NULL));
+//	setlocale(LC_ALL, "rus");
+//	int a,sum=0;
+//	cout << "Введите число, неважно сколько знаков - ";
+//	cin >> a;
+//	for (int i = a; i != 0 ; i/=10) {
+//		int abc = i % 10;
+//		sum += abc;
+//	}
+//	cout << "Сумма цифр числа = " << sum << endl;
+//
+//}
+
+//int main()
+//{
+//	srand(time(NULL));
+//	setlocale(LC_ALL, "rus");
+//	int indexMin = 0;
+//	int min = 10 + rand() % (81);
+//	for (int i = 0,k; i < 9; i++) {
+//		k = 10 + rand() % (81);
+//		cout << k << " ";
+//		if (k < min) {
+//			min = k;
+//			indexMin = i;
+//		}
+//	}
+//	cout << endl <<  min << " index = " << indexMin << endl;
+//}
+
+//int main() // создать массив из 10 рандомных чисел, с числами от 10 до 90, вывести значение этого массива + сумму чисел всего массива
+//{
+//	srand(time(NULL));
+//	setlocale(LC_ALL, "rus");
+//	int array[10],sum=0;
+//	for (int i = 0; i < 10; i++) {
+//		array[i] = 1 + rand() % (99);
+//		cout << array[i] << " ";
+//		sum += array[i];
+//	}
+//	cout << endl;
+//	cout << "Сумма - " << sum << endl;
+//}
+
+int main() // создать массив из 10 рандомных чисел, с числами от 10 до 90, вывести значение этого массива
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "rus");
+	int array[10],a,b,count=0,indexMin=0,indexMax=0;
+	float sum = 0;
+	cout << "Введите диапазон - ";
+	cin >> a >> b;
+	int min = b;
+	int max = a;
+	if (a > b) swap(a, b);
+	for (int i = 0; i < 10; i++) {
+		array[i] = a + rand() % (b+1-a);
+		if (min > array[i]) {
+			min = array[i];
+			indexMin = i;
+		}
+		if (max < array[i]) {
+			max = array[i];
+			indexMax = i;
+		}
+		cout << array[i] << " ";
+		sum += array[i];
+
+	}
+	cout << endl;
+	cout << "summ = " << sum << endl;
+	cout << "crednee arifm = " << sum/10 << endl;
+	cout << "min znachenie = " << min << endl;
+	cout << "max znachenie = " << max << endl;
+	cout << "index min znacheniya = " << indexMin << endl;
+	cout << "index max znacheniya = " << indexMax << endl;
+}
---
+++ lesson_for2.cpp
@@ -0,0 +1,234 @@
+﻿#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a;
+//	int n;
+//	cout << "Введите символ - ";
+//	cin >> a;
+//	cout << "Введите число n -  ";
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cout << " " << endl;
+//		for (int j = 0; j < n; j++) {
+//			cout << a << " ";
+//		}
+//	}
+//
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a;
+//	int w,h;
+//	cin >> a >> w >> h;
+//	for (int i = 0; i < h; i++) {
+//		for (int j = 0; j < w; j++) {
+//			cout << a << " ";
+//		}
+//		cout << endl;
+//	}
+//	
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a;
+//	int n;
+//  cout << "Введите символ и число n - ";
+//	cin >> a >> n;
+//	for (int i = 0; i < n; i++) {
+//		cout << " " << endl;
+//			for (int j = 0; j < n; j++) {
+//				cout << a << " ";
+//			}
+//	}
+//
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");                                                        // вывод пустого квадрата
+//	char a = '*';
+//	int n;
+//	cout << "Введите число n - ";
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cout << " " << endl;
+//		for (int j = 0; j < n; j++) {
+//			if (i == 0 || i == n-1 || j == 0 || j == n-1) cout << a << " ";
+//			else cout << "  ";
+//		}
+//	}
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");                                                         // вывод квадрата с крестом посередине
+//	char a = '*';
+//	int n;
+//	cout << "Введите число n - ";
+//	cin >> n;
+//	if (n % 2 == 0) n++;
+//	for (int i = 0; i < n; i++) {
+//		cout << a << " ";
+//	}
+//	cout << endl;
+//	for (int i = 0; i < n - 2; i++) {
+//		cout << "* ";
+//		for (int j = 0; j < n - 2; j++) {
+//			if (i == (n - 2) / 2 || j == (n - 2) / 2)
+//				cout << "* ";
+//			else cout << "  ";
+//		}
+//		cout << "*" << endl;
+//	}
+//	for (int i = 0; i < n; i++) {
+//		cout << a << " ";
+//	}
+//}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");                                                         // вывод заполненного квадрата с крестом посередине
+//	char a = '*';
+//	int n;
+//	cout << "Введите число n - ";
+//	cin >> n;
+//	if (n % 2 == 0) n++;
+//	for (int i = 0; i < n; i++) {
+//		cout << a << " ";
+//	}
+//	cout << endl;
+//	for (int i = 0; i < n - 2; i++) {
+//		cout << "* ";
+//		for (int j = 0; j < n - 2; j++) {
+//			if (i == (n - 2) / 2 || j == (n - 2) / 2)
+//				cout << "* ";
+//			else cout << "  ";
+//		}
+//		cout << "*" << endl;
+//	}
+//	for (int i = 0; i < n; i++) {
+//		cout << a << " ";
+//	}
+//}
+
+//int main()                                                         // [1:1] [1:2] [1:3]
+//														           
+//{
+//	setlocale(LC_ALL, "rus");
+//	for (int i = 1; i <= 3; i++) {
+//		cout << " \n";
+//		for (int j = 1; j <= 3; j++) {
+//			cout << "[" << i << ":" << j << "]";
+//		}
+//	}
+//}
+
+//int main()                                                         // вывод перевернутого полуквадрата
+//																   
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a = '*';
+//	int b;
+//	cin >> b;
+//	for (int i = 1; i <= b; i++) {
+//		cout << " " << endl;
+//		for (int j = 5; j > i; j--) {
+//			cout << a;
+//		}
+//	}
+//}
+
+//int main()                                                         // вывод полуквадрат
+//																   
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a = '*';
+//	int b;
+//	cin >> b;
+//	for (int i = 1; i <= b; i++) {
+//		cout << " " << endl;
+//		for (int j = b; j > i; j--) {
+//			cout << a;
+//		}
+//	}
+//}
+
+//int main()                                                         // вывод полуквадрат обратный
+//
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a = '*';
+//	int size;
+//	cin >> size;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size - i - 1; j++) {
+//			cout << " ";
+//		}
+//		for (int j = 0; j < i+1; j++) {
+//			cout << a;
+//		}
+//		cout << endl;
+//	}
+//}
+
+//int main()                                                         // вывод полуквадрат обратный x1
+//
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a = '*';
+//	int size;
+//	cin >> size;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size - i - 1; j++) {
+//			cout << " ";
+//		}
+//		for (int j = 0; j < i + 1; j++) {
+//			cout << a;
+//		}
+//		cout << endl;
+//	}
+//
+//}
+
+//int main()                                                         // вывод полуквадрат обратный x2
+//
+//{
+//	setlocale(LC_ALL, "rus");
+//	char a = '*';
+//	int size;
+//	cin >> size;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < i + 1; j++) {
+//			cout << " ";
+//		}
+//		for (int j = 0; j < size-i; j++) {
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//}
+
+int main()                                                         // вывод грусть ;(
+
+{
+	setlocale(LC_ALL, "rus");
+	char a = '*';
+	int size;
+	cin >> size;
+	for (int i = 0; i <= size/2; i++) {
+		for (int j = 0; j < size/2-i; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < i*2+1; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
